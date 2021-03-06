7564b92e585256cf27347f81d717ccd283ec5af2
Jason <Jason.patino94@gmail.com>
2021-03-06 00:01:24 -0500

    commit tree ea3c395765198e9b037b7f476a651152e23472f4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+// const link = {
+//   width: "100px",
+//   padding: "12px",
+//   margin: "0 6px 6px",
+//   background: "blue",
+//   textDecoration: "none",
+//   color: "white",
+// };
+
+// const Navbar = () => (
+//   <div>
+//     <NavLink to="/" exact style={link} activeStyle={{ background: "darkblue" }}>
+//       Home
+//     </NavLink>
+//     <NavLink
+//       to="/about"
+//       exact
+//       style={link}
+//       activeStyle={{ background: "darkblue" }}
+//     >
+//       About
+//     </NavLink>
+//     <NavLink
+//       to="/login"
+//       exact
+//       style={link}
+//       activeStyle={{ background: "darkblue" }}
+//     >
+//       Login
+//     </NavLink>
+//   </div>
+// );
+
+// // const Home = () => {
+// //   return (
+// //     <div>
+// //       <h1>Home!</h1>
+// //     </div>
+// //   );
+// // };
+// // THIS IS THE SAME AS THE FUNCTION BELOW!
+
+// const Home = () => <h1>Home!</h1>;
+
+// // const About = () => {
+// //   return (
+// //     <div>
+// //       <h1> This is about my component!</h1>
+// //     </div>
+// //   );
+// // };
+// // THIS IS THE SAME AS THE FUNCTION BELOW!
+
+// const About = () => <h1>This is about my component!</h1>;
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <label htmlFor="username">Username: </label>
+//           <input type="text" name="username" placeholder="Username" />
+//         </div>
+//         <div>
+//           <label htmlFor="password">Password: </label>
+//           <input type="password" name="password" placeholder="Password" />
+//         </div>
+//         <input type="submit" value="login" />
+//       </form>
+//     </div>
+//   );
+// };
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
